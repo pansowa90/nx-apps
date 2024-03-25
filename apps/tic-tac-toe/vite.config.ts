346b440191a7bf5697c6b38e8764183767bd7dec
pansowa90 <b.tomaszewski@icloud.com>
2024-03-25 12:25:37 +0100

    Update vite.config.ts

--- apps/tic-tac-toe/vite.config.ts
+++ apps/tic-tac-toe/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import path from 'path';
 
 export default defineConfig({
-  // root: __dirname,
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/tic-tac-toe',
 
   resolve: {
@@ -37,6 +37,11 @@ export default defineConfig({
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
     },
   },
 
