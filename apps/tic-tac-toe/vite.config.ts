94d0d104f788c5277bcb2809ee1a367f12aa31fd
pansowa90 <b.tomaszewski@icloud.com>
2024-03-25 11:43:53 +0100

    Update vite.config.ts

--- apps/tic-tac-toe/vite.config.ts
+++ apps/tic-tac-toe/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import path from 'path';
 
 export default defineConfig({
-  // root: __dirname,
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/tic-tac-toe',
 
   resolve: {
