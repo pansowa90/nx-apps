84d83bd9cb327f16ba74abff62289b75a40221fa
pansowa90 <b.tomaszewski@icloud.com>
2024-03-28 11:32:11 +0100

    Update magicContainer.tsx

--- apps/magic-icon/src/component/magicContainer.tsx
+++ apps/magic-icon/src/component/magicContainer.tsx
@@ -1,7 +1,7 @@
 import { useRef, useState, MouseEvent } from "react";
 import MagicIcon, { IconType } from "../component/magicIcon";
 
-export default function MagicContainer({ icon }: { icon: IconType }) {
+export default function MagicContainer({ icon, custom }: { icon: IconType; custom: number; }) {
   const containerRef = useRef<HTMLDivElement>(null);
   const [cursor, setCursor] = useState({ x: 0, y: 0 });
   const [mouseOnContainer, setMouseOnContainer] = useState(false);
@@ -23,7 +23,13 @@ export default function MagicContainer({ icon }: { icon: IconType }) {
       onMouseEnter={() => setMouseOnContainer(true)}
       onMouseLeave={() => setMouseOnContainer(false)}
       className="flex place-items-center justify-center w-[24] h-[24] p-8 stroke-[.2] hover:stroke-[.25] cursor-pointer">
-      <MagicIcon icon={icon} cursor={cursor} containerRef={containerRef} mouseOnContainer={mouseOnContainer} />
+      <MagicIcon 
+        icon={icon} 
+        cursor={cursor} 
+        containerRef={containerRef} 
+        mouseOnContainer={mouseOnContainer} 
+        custom={custom}
+      />
     </div>
   );
 }
