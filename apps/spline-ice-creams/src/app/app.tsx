45285fd97a7209c8250634a76ddd961c4148dc7c
pansowa90 <b.tomaszewski@icloud.com>
2024-03-31 18:13:02 +0200

    update spline-ice-creams app

--- apps/spline-ice-creams/src/app/app.tsx
+++ apps/spline-ice-creams/src/app/app.tsx
@@ -17,10 +17,10 @@ const titleVariants = {
 export default function App() {
   return (
     <div className='flex flex-col lg:flex-row w-full h-lvh lg:items-center'>
-      <div className='absolute flex items-centerjustify-items-center text-gray-50 w-144 h-44 text-4xl lg:text-7xl p-12 rounded-sm pointer-events-none'>
+      <div className='absolute z-1 flex items-centerjustify-items-center text-gray-50 w-144 h-44 text-4xl lg:text-7xl p-12 rounded-sm pointer-events-none'>
         <motion.span variants={titleVariants} initial="start" animate="end"> Hello there 👋</motion.span>
       </div>
-      <Suspense fallback={<div className='absolute flex w-full items-center place-content-center text-7xl'>...</div>}>
+      <Suspense fallback={<div className='absolute flex w-full items-center place-content-center text-gray-50 text-7xl'>...</div>}>
         <Spline scene="https://prod.spline.design/H5MjA0Gq5MXYlhZw/scene.splinecode" />
       </Suspense>
 
