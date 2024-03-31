af26a9979c1a13c5b6186d7438008c578a1efd04
pansowa90 <b.tomaszewski@icloud.com>
2024-03-31 18:05:56 +0200

    init spline-ice-creams app

---
+++ apps/spline-ice-creams/tailwind.config.js
@@ -0,0 +1,21 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {}
+    },
+  },
+  plugins: [],
+};
