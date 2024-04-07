bf0106034297cfb1c9e783b32c908b842f634897
pansowa90 <b.tomaszewski@icloud.com>
2024-04-07 18:52:14 +0200

    update | nextjs-dashboard | chapter 16 - customers

--- apps/nextjs-dashboard/app/dashboard/customers/page.tsx
+++ apps/nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-  return (<p>Customers page</p>);
+import { fetchInvoicesPages } from '@/app/lib/data';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { lusitana } from '@/app/ui/fonts';
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import CustomersTable from '@/app/ui/customers/table';
+
+export default async function Page({ searchParams }: {
+  searchParams?: {
+    page?: string;
+    query?: string;
+  }
+}) {
+
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </main>
+  );
 }
